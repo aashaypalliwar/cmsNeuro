503c000c0f159b6e643a2134eacd9caeba75e94e
p_ash <palliwar.aashay@gmail.com>
2020-08-14 21:25:04 +0530

    Reformatted server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const app = require("./app");
+const comments = require("./comments/Comments.js");
 const { PORT } = require("./utils/config");
 require("./model/dbModel/database");
-const comments = require("./comments/Comments.js");
 
 const server = app.listen(PORT, () => {
   console.log(`server is running on ${PORT} ...`);
 });
 
+//Set up socket for comments
 comments.setupSocket(server);
-/**
- * 1. Require app       -done
- * 2. Set up server     -done
- * 3. Set up socket
- */
+
