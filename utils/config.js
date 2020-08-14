184700c60968eb8717f12a5900a096cdf8b571ac
p_ash <palliwar.aashay@gmail.com>
2020-08-14 21:26:18 +0530

    Removed comments

--- utils/config.js
+++ utils/config.js
@@ -8,5 +8,3 @@ exports.JWT_SECRET = process.env.JWT_SECRET ;
 exports.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN
 exports.JWT_COOKIE_EXPIRES_IN = process.env.JWT_COOKIE_EXPIRES_IN ;
 
-
-//Add and remove as required.
