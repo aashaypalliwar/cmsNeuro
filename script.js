f6b33d60e57f4392d12c316916aaca932e0b8266
Shrirang <deshmukhshrirang01@gmail.com>
2020-12-29 18:59:42 +0530

    Some Changes to Script.js

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 //This file was written only to autenticate users using fake emails bulkly
 
-const express = require("express");
-const app = require('./app');
-const bcrypt = require('bcryptjs');
-const db = require('./model/dbModel/database');
-const authController = require('./controller/authController');
+const bcrypt = require("bcryptjs");
+const db = require("./model/dbModel/database");
 
-exports.fakeSignup  = async(req,res,next) =>{
-    try {
-        const name = "binod Super";
-    const role = "admin";
-    const password = "12345";
-    let email;
+exports.secySignup = async (req, res, next) => {
+  try {
+    for (i = 0; i < 7; i++) {
+      const name = `Dummy User ${i}`;
+      const role = "user";
+      const password = "CMS_Neuro";
+      const email = `${i}@iitbbs.ac.in`;
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const timeStamp = Date.now();
+      const designation = "Dummy";
 
-    for (let i = 14; i < 18 ;i++) {
-        email = `${i+1}@iitbbs.ac.in`;
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const timeStamp = Date.now();
+      const queryParams = [
+        name,
+        email,
+        role,
+        timeStamp,
+        hashedPassword,
+        designation,
+      ];
 
-        const queryParams = [name,email,role,timeStamp,hashedPassword,"core"];
-
-        await db.query(
-            `INSERT INTO users (name, email, role,timestamp, password,designation) VALUES (?, ?, ?,?,?,?)`,
-            queryParams
-          );
-          console.log(i+1);
-        
+      await db.query(
+        `INSERT INTO users (name, email, role,timestamp, password,designation) VALUES (?, ?, ?,?,?,?)`,
+        queryParams
+      );
     }
-    console.log("Hello World");
-    } catch (error) {
-       console.log(error); 
-    }
-} 
+    res.status(200).json({
+      message: "Secy Added Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
